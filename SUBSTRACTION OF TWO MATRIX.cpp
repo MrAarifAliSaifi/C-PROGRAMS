cf4cbe0b2e0e56d171420c4405bdf72878d6f25a
Aarif Ali Saifi <67059471+MrAarifAliSaifi@users.noreply.github.com>
2020-07-09 15:45:28 +0530

    SUBSTRACTION OF TWO MATRIX

---
+++ SUBSTRACTION OF TWO MATRIX.cpp
@@ -0,0 +1,39 @@
+#include<stdio.h>
+int main()
+{
+	int i,a[3][3],b[3][3],c[3][3],j;
+	printf("PLEASE ENTER THE ELEMENTS OF THE FIRST MATRIX");
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			scanf("%d",&a[i][j]);
+		}
+	}
+	printf("PLEASE ENTER THE ELEMENTS OF THE SECOND METRIX MATRIX");
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			scanf("%d",&b[i][j]);
+		}
+	}
+	
+	printf("\nTHE RESULTED MATRIX:\n");
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			c[i][j]=a[i][j]-b[i][j];
+		}
+		
+	}
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			printf("\t%d",c[i][j]);
+		}
+		printf("\n");
+	}
+}
